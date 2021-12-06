edf5ea18361bbb7132bb827855ff43588a72c651
Andrew <yanovi4.a@gmail.com>
2021-12-06 20:10:27 +0100

    304 - Added a LocationInput and the 'Location' package

--- lib/screens/add_place_screen.dart
+++ lib/screens/add_place_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../widgets/image_input.dart';
 import '../providers/great_places.dart';
+import '../widgets/location_input.dart';
 
 class AddPlaceScreen extends StatefulWidget {
   static const routeName = '/add-place';
@@ -52,6 +53,8 @@ class _AddPlaceScreenState extends State<AddPlaceScreen> {
                     ),
                     const SizedBox(height: 10),
                     ImageInput(onSelectImage: _selectImage),
+                    const SizedBox(height: 10),
+                    LocationInput(),
                   ],
                 ),
               ),
