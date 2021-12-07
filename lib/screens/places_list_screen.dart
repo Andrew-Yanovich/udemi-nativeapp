2e269dbf80466163e3fd79ccb356c6eaa3c8e097
Andrew <yanovi4.a@gmail.com>
2021-12-07 14:47:15 +0100

    310 - Added a 'PlaceDetail' Screen. Opening the Map in ReadOnly mode.

--- lib/screens/places_list_screen.dart
+++ lib/screens/places_list_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../providers/great_places.dart';
 import './add_place_screen.dart';
+import './place_detail_screen.dart';
 
 class PlacesListScreen extends StatelessWidget {
   const PlacesListScreen({Key? key}) : super(key: key);
@@ -41,9 +42,13 @@ class PlacesListScreen extends StatelessWidget {
                                 FileImage(greatPlaces.items[i].image),
                           ),
                           title: Text(greatPlaces.items[i].title),
-                          subtitle: Text(greatPlaces.items[i].location!.address!),
+                          subtitle:
+                              Text(greatPlaces.items[i].location!.address!),
                           onTap: () {
-                            // Go to detail page
+                            Navigator.of(context).pushNamed(
+                              PlaceDetailScreen.routeName,
+                              arguments: greatPlaces.items[i].id,
+                            );
                           },
                         ),
                       ),
